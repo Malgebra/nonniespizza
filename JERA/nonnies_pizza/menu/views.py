acd4ff02059aeca116f594c785eff1c85759a1d7
BS32 <eguzmandls@gmail.com>
2023-04-07 20:44:47 -0400

    Menu Template created

--- JERA/nonnies_pizza/menu/views.py
+++ JERA/nonnies_pizza/menu/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import Item
 
 def menu(request):
-    return HttpResponse("Menu")
-    
+    template = loader.get_template('menu.html')
+    myitems = Item.objects.all().values()
+
+    context = {
+        'myitems': myitems,
+    }
+    return HttpResponse(template.render(context, request))
