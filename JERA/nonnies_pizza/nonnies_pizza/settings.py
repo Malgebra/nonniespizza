29cd70a9fbe8a026f763c88bd72e93955fdfbded
BlackHeart32 <eguzmandls@gmail.com>
2023-04-24 15:14:08 -0400

    I'm taking the menu

--- JERA/nonnies_pizza/nonnies_pizza/settings.py
+++ JERA/nonnies_pizza/nonnies_pizza/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'menu'
+    'bootstrap5',
+    'menu',
 ]
 
 MIDDLEWARE = [
