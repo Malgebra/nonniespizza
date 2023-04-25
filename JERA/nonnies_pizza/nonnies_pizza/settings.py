b980eac2e7a53ba428c9592ea30d37de88b37bff
BlackHeart32 <eguzmandls@gmail.com>
2023-04-24 20:26:09 -0400

    changed database to mysql

--- JERA/nonnies_pizza/nonnies_pizza/settings.py
+++ JERA/nonnies_pizza/nonnies_pizza/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'nonnies_pizza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'spring2023',
+        'HOST': 'localhost',
+        'PORT': '3306'
+
     }
 }
 
