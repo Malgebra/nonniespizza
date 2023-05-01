3b56a5af5ce50df60bf00522abbbc12df1f4779f
Andrew Anchor <andrewanchor@aol.com>
2023-05-01 14:57:47 -0400

    Changing database back to SQLite

--- JERA/nonnies_pizza/menu/models.py
+++ JERA/nonnies_pizza/menu/models.py
@@ -3,3 +3,9 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
